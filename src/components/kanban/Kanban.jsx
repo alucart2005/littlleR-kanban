422ae074faa73ec73e3872ca5700892c4bff2b42
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-09-08 18:14:28 -0500

    feat: ok

--- src/components/kanban/Kanban.jsx
+++ src/components/kanban/Kanban.jsx
@@ -32,7 +32,39 @@ export function Kanban() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
         {data.map((section) => (
-          <Droppable key={section.id} droppableId={section.id}></Droppable>
+          <Droppable key={section.id} droppableId={section.id}>
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                className="kanban_section"
+                ref={provided.innerRef}
+              >
+                <div className="kanban__section__title">{section.title}</div>
+                <div className="kanban__section__content">
+                  {section.task.map((task,index)=>(
+                    <Draggable 
+                    key={task.id} 
+                    draggableId={task.id}
+                    index={index}
+                    >
+                      {(provided,snapshot)=>(
+                        <div 
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={
+                          {...provided.dragHandleProps.style,opacity:snapshot.isDragging?"0.5":"1"}
+                        }
+                        >
+                          <Card>{task.title}</Card>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              </div>
+            )}
+          </Droppable>
         ))}
       </div>
     </DragDropContext>
