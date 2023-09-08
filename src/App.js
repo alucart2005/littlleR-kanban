70664ef464eea8b78e381fe2bf607fd1b571de0b
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-09-08 16:55:19 -0500

    feat: on errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import "./mockData"
-import mockData from './mockData';
+import "./App.css";
+import "./mockData";
+import mockData from "./mockData";
+import { Kanban } from "./components/kanban/Kanban";
 
 function App() {
   return (
-    <div style={{padding:"50px"}}>
-      <h1 style={{marginTop:'20px', color:'blue'}}>Kanban UI</h1>
+    <div style={{ padding: "50px" }}>
+      <h1 style={{ marginTop: "20px", color: "blue" }}>Kanban UI</h1>
+      <Kanban></Kanban>
+
       {console.log(mockData)}
     </div>
   );
--- src/components/kanban/Kanban.jsx
+++ src/components/kanban/Kanban.jsx
@@ -1,24 +1,40 @@
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import mockData from "../../mockData";
-import {Card} from "../card/Card"
+import { Card } from "../card/Card";
 import { useState } from "react";
 
 export function Kanban() {
-  const [data, setData] = useState(mockData)
-  const onDragEnd=(result)=>{
-    if(!result.destination) return
-    const{source,destination} = result
-    if(source.droppableId !==destination.droppableId){
-      const sourceColIndex=data.findIndex(e=>e.id===source.droppableId)
-      const destinationColIndex=data.findIndex(e=>e.id===destination.droppableId)
-      const sourceCol = data[sourceColIndex]
-      const destinationCol = data[destinationColIndex]
+  const [data, setData] = useState(mockData);
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+      const destinationColIndex = data.findIndex(
+        (e) => e.id === destination.droppableId
+      );
+      const sourceCol = data[sourceColIndex];
+      const destinationCol = data[destinationColIndex];
 
-      const sourceTask = [...sourceCol.task]
-      const destinationTask = [...destinationCol.task]
+      const sourceTask = [...sourceCol.task];
+      const destinationTask = [...destinationCol.task];
+
+      const [remove] = sourceTask.splice(source.index, 1); //disappear origin task
+      destinationTask.splice(destination.index, 0, remove);
+
+      data[sourceColIndex].task = sourceTask; // data update
+      data[destinationColIndex].task = destinationTask;
+
+      setData(data);
     }
-  }
+  };
   return (
-
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        {data.map((section) => (
+          <Droppable key={section.id} droppableId={section.id}></Droppable>
+        ))}
+      </div>
+    </DragDropContext>
   );
 }
